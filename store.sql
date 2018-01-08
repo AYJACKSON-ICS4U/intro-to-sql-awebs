a0affd57be2cd69ac11d9cc4a541179652ee8151
awebs <awebs1@ocdsb.ca>
2018-01-08 18:32:27 -0500

    Basic commit of the store design

---
+++ store.sql
@@ -0,0 +1,8 @@
+CREATE TABLE products (id INTEGER PRIMARY KEY, item TEXT, price INTEGER);
+
+INSERT INTO products VALUES (1, "", 1);
+
+SELECT * FROM products;
+
+SELECT * FROM products ORDER BY price;
+SELECT * FROM products
